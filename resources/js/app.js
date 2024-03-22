cf058a8680a0ab777de0da3da2829b4291a5ef80
Xavi <jciocci@outlook.com>
2024-03-22 10:59:11 -0400

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,6 @@ import './bootstrap';
 import DataTable from 'datatables.net-dt';
 
 document.addEventListener('DOMContentLoaded', function() {
-    //datatables
     let tableElement = document.querySelector('.dt');
     let table = new DataTable(tableElement, {
         pageLength: 50
@@ -14,19 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all forms with the class 'form'
     const forms = document.querySelectorAll('.form');
-
     forms.forEach(function(form) {
         form.addEventListener('submit', function(event) {
-            // Prevent the form from submitting for demonstration purposes
-            // Remove this line if you want the form to actually submit
-            //event.preventDefault(); 
-
-            // Find all button elements within the form
             const buttons = form.querySelectorAll('button');
-
-            // Disable and style buttons to appear grayed out
             buttons.forEach(function(button) {
                 button.disabled = true; // Disable the button
                 button.classList.add('disabled-button'); // Add a class to style the button (optional)
